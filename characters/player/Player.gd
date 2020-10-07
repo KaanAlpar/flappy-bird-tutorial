a4ed9d348b4316c533e5d79afe65e709cdc6789e
Kaan Alpar <kaanalpar33@gmail.com>
2020-10-07 17:34:58 +0300

    part 3 - player script

---
+++ characters/player/Player.gd
@@ -0,0 +1,35 @@
+extends RigidBody2D
+
+export var FLAP_FORCE = -200
+
+const MAX_ROTATION_DEGREES = -30.0
+
+onready var animator = $AnimationPlayer
+
+var started = false
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("flap"):
+		if !started:
+			start()
+		flap()
+	
+	if rotation_degrees <= MAX_ROTATION_DEGREES:
+		angular_velocity = 0
+		rotation_degrees = MAX_ROTATION_DEGREES
+	
+	if linear_velocity.y > 0:
+		if rotation_degrees <= 90:
+			angular_velocity = 3
+		else:
+			angular_velocity = 0
+
+func start():
+	if started: return
+	started = true
+	gravity_scale = 5.0
+	animator.play("flap")
+
+func flap():
+	linear_velocity.y = FLAP_FORCE
+	angular_velocity = -8.0
